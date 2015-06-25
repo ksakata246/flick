97ff35f2999cac478d86c94b762a5ca6428a7a81
坂田和輝 <k.sakata246@gmail.com>
2015-06-25 21:41:52 +0900

    検索部分の疎通

--- Flick/src/jp/co/se/android/recipe/chapter03/MyListFragment.java
+++ Flick/src/jp/co/se/android/recipe/chapter03/MyListFragment.java
@@ -1,5 +1,7 @@
 package jp.co.se.android.recipe.chapter03;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.widget.ArrayAdapter;
@@ -9,10 +11,9 @@ public class MyListFragment extends ListFragment {
 	private boolean isFirst = false;
 	private ArrayAdapter<UrlSearchResultVO> usrAdapter;
 
-
 	public MyListFragment() {
 	};
-	
+
 	// これを使ってやってみる
 	public MyListFragment(ArrayAdapter<UrlSearchResultVO> adapter) {
 		isFirst = true;
@@ -29,10 +30,17 @@ public class MyListFragment extends ListFragment {
 					MyList.Data);
 			setListAdapter(adapter);
 		} else {
-			//ここを変える
+			ArrayList<String> usrData = new ArrayList<String>();
+			String[] data = new String[100];
+			for (int i = 0; i < 10; i++) {
+				String a = usrAdapter.getItem(i).getTitle();
+				usrData.add(usrAdapter.getItem(i).getTitle());
+				data[i] = a;
+			}
+			// ここを変える
 			ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(
 					getActivity(), android.R.layout.simple_list_item_1,
-					MyList1.Data);
+					data);
 			setListAdapter(adapter1);
 		}
 	}
